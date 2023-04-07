247cb7e8eff65659d3c2a50ebd500f862562dee1
john248lee <fantasy10final@gmail.com>
2023-04-07 17:28:47 +0800

    Shift Reiceive v1.0 AND MathV v0.1
    1. Update the BallController script to strengthen the shift reiceive function. Now the player can only shift reiceive the ball in a particular range. Game developers can modify the start angle and the angle range of the catch-enabled area. The area is represented by a purple Handle.DrawSolidArc
    2. Add the function library MathV, which is a library that deal with Vector3. Now included cross, and isVectorBetween to determine if a target vector falls within a given rotation range defined by two boundary vectors.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class BallController : MonoBehaviour
 {
-    public float jumpForce = 10f;
-    public float closeDistance = 2f;
+    public float shiftReceiveForce = 10f;
+    public float shiftReceiveRadius = 2f;
     public Rigidbody2D ballRigidbody;
-    private bool canJump = false;
-
+    private bool canShiftReceive = false;
+    public float shiftReceiveFromAngle = -45f;
+    public float shiftRangeAngle = 90f;
+    public Vector3 shiftReceiveFromVector;
+    public Vector3 shiftReceiveToVector;
+    private void Start()
+    {
+        this.shiftReceiveFromVector = Quaternion.AngleAxis(shiftReceiveFromAngle, Vector3.forward) * transform.right;
+        this.shiftReceiveToVector = Quaternion.AngleAxis(shiftRangeAngle, Vector3.forward) * shiftReceiveFromVector;
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canJump)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canShiftReceive)
         {
             ballRigidbody.velocity = new Vector2(0f, 0f);
-            ballRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            canJump = false;
+            ballRigidbody.AddForce(Vector2.up * shiftReceiveForce, ForceMode2D.Impulse);
+            canShiftReceive = false;
         }
     }
 
     private void FixedUpdate()
     {
         float distance = Vector2.Distance(transform.position, ballRigidbody.transform.position);
-        if (distance <= closeDistance)
-        {
-            canJump = true;
-        }
+        Vector3 ballDir = ballRigidbody.transform.position - transform.position;
+        canShiftReceive = (distance <= shiftReceiveRadius && MathV.isVectorBetween(shiftReceiveFromVector, shiftReceiveToVector, ballDir));
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(BallController))]
+public class BallControllerEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        BallController linkedObject = target as BallController;
+        linkedObject.shiftReceiveFromVector = Quaternion.AngleAxis(linkedObject.shiftReceiveFromAngle, Vector3.forward) * linkedObject.transform.right;
+        linkedObject.shiftReceiveToVector = Quaternion.AngleAxis(linkedObject.shiftRangeAngle, Vector3.forward) * linkedObject.shiftReceiveFromVector;
+        Handles.color = new Color(1, 0, 1, 0.5f);
+        Handles.DrawSolidArc(linkedObject.transform.position, new Vector3(0, 0, 1), linkedObject.shiftReceiveFromVector, linkedObject.shiftRangeAngle, linkedObject.shiftReceiveRadius);
+        
+    }
+}
+#endif
