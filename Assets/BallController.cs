4f36ec6b5145123d8e0a6701c7994149ae8e38ac
john248lee <fantasy10final@gmail.com>
2023-04-08 23:13:57 +0800

    BallController v1.2
    Tidy up the inspector. Create a button called "Modify Shift Receive" in the inspector. The settings related to the shift receiving function appears only if I click the button.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,19 +5,21 @@ using UnityEditor;
 
 public class BallController : MonoBehaviour
 {
-    public float shiftReceiveForce = 10f;
-    public float shiftReceiveRadius = 2f;
+    [HideInInspector] public float shiftReceiveForce = 10f;
+    [HideInInspector] public float shiftReceiveRadius = 2f;
     public Rigidbody2D ballRigidbody;
     private GluedBool canShiftReceive = new GluedBool();
-    public float shiftReceiveFromAngle = -45f;
-    public float shiftRangeAngle = 90f;
-    public Vector3 shiftReceiveFromVector;
-    public Vector3 shiftReceiveToVector;
+    [HideInInspector] public float shiftReceiveFromAngle = -45f;
+    [HideInInspector, Range(0f, 359f)] public float shiftRangeAngle = 90f;
+    [HideInInspector] public Vector3 shiftReceiveFromVector;
+    [HideInInspector] public Vector3 shiftReceiveToVector;
+
     private void Start()
     {
         this.shiftReceiveFromVector = Quaternion.AngleAxis(shiftReceiveFromAngle, Vector3.forward) * transform.right;
         this.shiftReceiveToVector = Quaternion.AngleAxis(shiftRangeAngle, Vector3.forward) * shiftReceiveFromVector;
     }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift) && canShiftReceive.value())
@@ -40,14 +42,49 @@ public class BallController : MonoBehaviour
 [CustomEditor(typeof(BallController))]
 public class BallControllerEditor : Editor
 {
-    private void OnSceneGUI()
+    public static bool showShiftReceiveSettings = false;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BallController linkedObject = target as BallController;
+
+        if (GUILayout.Button(showShiftReceiveSettings ? "Hide Shift Receive" : "Modify Shift Receive"))
+        {
+            showShiftReceiveSettings = !showShiftReceiveSettings;
+        }
+        
+        if (showShiftReceiveSettings)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            float newShiftReceiveForce = EditorGUILayout.FloatField("Shift Receive Force", linkedObject.shiftReceiveForce);
+            float newShiftReceiveRadius = EditorGUILayout.FloatField("Shift Receive Radius", linkedObject.shiftReceiveRadius);
+            float newShiftReceiveFromAngle = EditorGUILayout.FloatField("Shift Receive From Angle", linkedObject.shiftReceiveFromAngle);
+            float newShiftRangeAngle = EditorGUILayout.Slider("Shift Range Angle", linkedObject.shiftRangeAngle, 0f, 359f);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(linkedObject, "Modify Shift Receive");
+                linkedObject.shiftReceiveForce = newShiftReceiveForce;
+                linkedObject.shiftReceiveRadius = newShiftReceiveRadius;
+                linkedObject.shiftReceiveFromAngle = newShiftReceiveFromAngle;
+                linkedObject.shiftRangeAngle = newShiftRangeAngle;
+                serializedObject.ApplyModifiedProperties();
+                EditorUtility.SetDirty(target);
+            }
+        }
+
+    }
+    public void OnSceneGUI()
     {
         BallController linkedObject = target as BallController;
-        linkedObject.shiftReceiveFromVector = Quaternion.AngleAxis(linkedObject.shiftReceiveFromAngle, Vector3.forward) * linkedObject.transform.right;
-        linkedObject.shiftReceiveToVector = Quaternion.AngleAxis(linkedObject.shiftRangeAngle, Vector3.forward) * linkedObject.shiftReceiveFromVector;
-        Handles.color = new Color(1, 0, 1, 0.5f);
-        Handles.DrawSolidArc(linkedObject.transform.position, new Vector3(0, 0, 1), linkedObject.shiftReceiveFromVector, linkedObject.shiftRangeAngle, linkedObject.shiftReceiveRadius);
-        
+        if (showShiftReceiveSettings)
+        {
+            linkedObject.shiftReceiveFromVector = Quaternion.AngleAxis(linkedObject.shiftReceiveFromAngle, Vector3.forward) * linkedObject.transform.right;
+            linkedObject.shiftReceiveToVector = Quaternion.AngleAxis(linkedObject.shiftRangeAngle, Vector3.forward) * linkedObject.shiftReceiveFromVector;
+
+            Handles.color = new Color(1, 0, 1, 0.5f);
+            Handles.DrawSolidArc(linkedObject.transform.position, new Vector3(0, 0, 1), linkedObject.shiftReceiveFromVector, linkedObject.shiftRangeAngle, linkedObject.shiftReceiveRadius);
+        }
     }
 }
 #endif
